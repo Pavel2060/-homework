a464f0290c7cdabb98547dd29839974317d5c5a2
Pavel2060 <shchennikov20070@list.ru>
2024-12-13 17:31:25 +0300

    '4commit'

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -30,9 +30,9 @@ export const HW3 = () => {
   return (
     <div id={'hw03'}>
       {currentText ? (
-        <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+        <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
-        <h1 id={'hw03-default-text'}>{currentText}</h1> 
+        <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> 
       )}
 
       <input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />
